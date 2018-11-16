2b8317af31801815c6c7911f4f0ee06c5fd8487c
Lereena <lereenadem@yandex.ru>
2018-11-16 11:54:21 +0300

    Initial commit

---
+++ SecretSatan.App/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretSatan.App
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Введите список имён через запятую");
+			string namesString;
+			while (string.IsNullOrWhiteSpace(namesString = Console.ReadLine())) ;
+			var names = namesString.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+			// call SatanRandomizer method
+			// visualize results
+			Console.ReadKey();
+		}
+	}
+}
---
+++ SecretSatan.Tests/SatanRandomizerTest.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SecretSatan.Tests
+{
+    [TestClass]
+    public class SatanRandomizerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(true, true);
+            Assert.AreNotEqual(true, false);
+            CollectionAssert.AreEquivalent(new[] {1, 2, 3}, new[] {3, 1, 2});
+        }
+
+        static bool NotEqual(Dictionary<string, string> pairs)
+        {
+            foreach (var pair in pairs)
+            {
+                if (pair.Key == pair.Value)
+                    return false;
+            }
+
+            return true;
+        }
+
+        static bool NotMatchingHunters(Dictionary<string, string> pairs)
+        {
+            foreach (var pair in pairs)
+            {
+                foreach (var pair2 in pairs)
+                    if (pair.Key == pair2.Key)
+                        return false;
+            }
+
+            return true;
+        }
+
+        static bool NotMatchingVictims(Dictionary<string, string> pairs)
+        {
+            foreach (var pair in pairs)
+            {
+                foreach (var pair2 in pairs)
+                    if (pair.Value == pair2.Value)
+                        return false;
+            }
+
+            return true;
+        }
+
+        static bool AllHaveVictims(Dictionary<string, string> pairs)
+        {
+            foreach (var pair in pairs)
+            {
+                if (string.IsNullOrEmpty(pair.Value.Trim()))
+                    return false;
+            }
+
+            return true;
+        }
+
+        static bool AllHaveHunters(Dictionary<string, string> pairs)
+        {
+            foreach (var pair in pairs)
+            {
+                if (string.IsNullOrEmpty(pair.Key.Trim()))
+                    return false;
+            }
+
+            return true;
+        }
+
+        static bool NotSpy(string[] people, Dictionary<string, string> pairs)
+        {
+            foreach (var pair in pairs)
+                if (!people.Contains(pair.Value) || !people.Contains(pair.Key))
+                    return false;
+            return true;
+        }
+
+        static bool IsEqualCounts(string[] people, Dictionary<string, string> pairs)
+        {
+            return (int) Math.Ceiling((double) (people.Length / 2)) == (double) pairs.Count;
+        }
+    }
+}
