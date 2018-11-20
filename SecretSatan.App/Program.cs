d8fe1ea77a253fde02bb7e0c4d9ccc9396b86e5c
Roman <42299959+DykemL@users.noreply.github.com>
2018-11-20 16:40:44 +0300

    Update Program.cs

--- SecretSatan.App/Program.cs
+++ SecretSatan.App/Program.cs
@@ -10,17 +10,17 @@ namespace SecretSatan.App
 	{
 		static void Main(string[] args)
 		{
-            Console.WriteLine("Введите список имён через запятую");
-			string namesString;
-			while (string.IsNullOrWhiteSpace(namesString = Console.ReadLine())) ;
-			var names = namesString.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+		    Console.WriteLine("Введите список имён через запятую");
+				string namesString;
+				while (string.IsNullOrWhiteSpace(namesString = Console.ReadLine())) ;
+				var names = namesString.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
 
-            Dictionary<string, string> pairs = SatanRandomizer.RandomizeSatanPairs(names);
-            foreach(var pair in pairs)
-            {
-                Console.WriteLine(pair.Key + " => " + pair.Value);
-            }
-            Console.ReadKey();
+		    Dictionary<string, string> pairs = SatanRandomizer.RandomizeSatanPairs(names);
+		    foreach(var pair in pairs)
+		    {
+			Console.WriteLine(pair.Key + " => " + pair.Value);
+		    }
+		    Console.ReadKey();
 		}
-    }
+    	}
 }
